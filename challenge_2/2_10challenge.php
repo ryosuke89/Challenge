fe3d1726e10ea10c6cdc699b1d4049159d0f95b7
ryosuke <ryosukekt89@gmail.com>
2015-11-11 10:54:12 +0900

    2_10challenge.phpを追加しました

--- challenge_2/2_10challenge.php
+++ challenge_2/2_10challenge.php
@@ -11,7 +11,7 @@ $key = $param;
 while ($key % 2 == 0 && $key > 2) {
   switch($key) {
      case $key:
-       $key = $key / 2 . ',';
+       $key = $key / 2;
        echo 2 . ',';
        break;
   }
@@ -20,7 +20,7 @@ while ($key % 2 == 0 && $key > 2) {
 while ($key % 3 == 0 && $key > 3) {
   switch($key) {
      case $key:
-       $key = $key / 3 . ',';
+       $key = $key / 3;
        echo 3 . ',';
        break;
   }
@@ -29,7 +29,7 @@ while ($key % 3 == 0 && $key > 3) {
 while ($key % 5 == 0 && $key > 5) {
   switch($key) {
      case $key:
-       $key = $key / 5 . ',';
+       $key = $key / 5;
        echo 5 . ',';
        break;
   }
@@ -38,7 +38,7 @@ while ($key % 5 == 0 && $key > 5) {
 while ($key % 7 == 0 && $key > 7) {
   switch($key) {
      case $key:
-       $key = $key / 7 . ',';
+       $key = $key / 7;
        echo 7 . ',';
        break;
   }
