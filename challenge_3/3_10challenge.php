9ae78159a33295588456f32eb30b12731c36b20b
ryosuke <ryosukekt89@gmail.com>
2015-11-15 16:11:51 +0900

    3_10challenge.phpを投稿しました。

---
+++ challenge_3/3_10challenge.php
@@ -0,0 +1,24 @@
+<?php
+
+function show_data($id){
+	$suzuki = array(1, '鈴木', '平成2年2月2日', '東京都');
+	$sato = array(2, '佐藤', '平成3年3月3日', null);
+	$yamada = array(3, '山田', '平成4年4月4日', '千葉県');
+
+		$member = array($suzuki, $sato, $yamada);
+		return $member[$id - 1];
+}
+
+$limit = 2;
+$array = array(1, 2, 3);
+foreach($array as $id){
+	foreach(show_data($id) as $key => $value){
+		if($key == 0 || $value == null){
+			continue;
+		}
+		echo "$value <br>";
+	}
+		if($id == $limit){
+			break;
+		}
+}
