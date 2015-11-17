556b7f91a68ba0f7813571f35b46c8c45f80dc38
ryosuke <ryosukekt89@gmail.com>
2015-11-17 16:17:28 +0900

    5_1challenge.phpを投稿しました。

---
+++ challenge_5/5_1challenge.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+ </head>
+ <body>
+  <form action="./5_2challenge.php" method="POST">
+  名前：<br>
+   <input type="text" name="txtName"><br><br>
+  性別：<br>
+   <input type="radio" name="rdoSample" value="男"> 男<br>
+   <input type="radio" name="rdoSample" value="女"> 女<br><br>
+  趣味：<br>
+   <textarea name="mulText"></textarea><br><br>
+   <input type="submit" name=btnSubmit">
+  </form>
+ </body>
+</html>
