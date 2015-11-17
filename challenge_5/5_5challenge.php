8ab716bd9a4eddaad8dedd638ad4e710d93b77d3
ryosuke <ryosukekt89@gmail.com>
2015-11-17 16:18:10 +0900

    5_5challenge.phpを投稿しました。

---
+++ challenge_5/5_5challenge.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+ </head>
+ <body>
+   <form enctype="multipart/form-data" action="5_6challenge.php" method="POST">
+   ファイルアップロード：<br><input name="userfile" type="file" /><br>
+    <input type="submit" name="btnSubmit">
+   </form>
+ </body>
+</html>
