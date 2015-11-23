947917d7c8dec09d15085c888c1b19acc13043f3
ryosuke <ryosukekt89@gmail.com>
2015-11-23 12:39:16 +0900

    7_7challenge.phpを投稿しました。

---
+++ challenge_7/7_7challenge.php
@@ -0,0 +1,28 @@
+<?php
+
+try{
+	$pdo_object=
+	new PDO('mysql:host=localhost;dbname=Challenge_db;charset=cp932','kato','kr890122');
+}catch(PDOException $Exception){
+	die('Ú‘±‚ÉŽ¸”s‚µ‚Ü‚µ‚½F'.$Exception->getMessage());
+}
+
+//XV
+$sql_update = "update profiles set name='¼‰ª C‘¢',age=48,birthday='1967-11-06' where profilesID=1";
+$query_update = $pdo_object->prepare($sql_update);
+$query_update -> execute();
+
+//•\Ž¦
+$sql = "select * from profiles where profilesID=1";
+$query = $pdo_object->prepare($sql);
+$query -> execute();
+
+while($row = $query->fetch(PDO::FETCH_OBJ)){
+	echo $row->profilesID . "<br>";
+	echo $row->name . "<br>";
+	echo $row->tell . "<br>";
+	echo $row->age . "<br>";
+	echo $row->birthday . "<br>" . "<br>";
+}
+
+$pdo_object = null;
