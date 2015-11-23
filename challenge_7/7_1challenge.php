2f54e7d0a733d2ff6287696dfd9ed9cf807a46f5
ryosuke <ryosukekt89@gmail.com>
2015-11-23 12:38:04 +0900

    7_1challenge.phpを投稿しました。

---
+++ challenge_7/7_1challenge.php
@@ -0,0 +1,12 @@
+<?php
+
+//エラーハンドリング
+try{
+	$pdo_object=
+	new PDO('mysql:host=localhost;dbname=Challenge_db;charset=cp932','kato','kr890122');
+}catch(PDOException $Exception){
+	die('接続に失敗しました：'.$Exception->getMessage());
+}
+
+//接続を切断
+$pdo_object = null;
