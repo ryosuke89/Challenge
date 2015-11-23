f9a33c58dee9dad6356b757e9da8a8dede223ee1
ryosuke <ryosukekt89@gmail.com>
2015-11-23 12:38:27 +0900

    7_3challenge.phpを投稿しました。

---
+++ challenge_7/7_3challenge.php
@@ -0,0 +1,22 @@
+<?php
+
+try{
+	$pdo_object=
+	new PDO('mysql:host=localhost;dbname=Challenge_db;charset=cp932','kato','kr890122');
+}catch(PDOException $Exception){
+	die('Ú‘±‚ÉŽ¸”s‚µ‚Ü‚µ‚½F'.$Exception->getMessage());
+}
+
+$sql = "select * from profiles";
+$query = $pdo_object->prepare($sql);
+$query -> execute();
+
+while($row = $query->fetch(PDO::FETCH_OBJ)){
+	echo $row->profilesID . "<br>";
+	echo $row->name . "<br>";
+	echo $row->tell . "<br>";
+	echo $row->age . "<br>";
+	echo $row->birthday . "<br>" . "<br>";
+}
+
+$pdo_object = null;
