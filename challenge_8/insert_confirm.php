345f59e77548ab695d95125b4e4700a35ba1e6b1
ryosuke <ryosukekt89@gmail.com>
2015-11-27 00:41:50 +0900

    insert_confirm.phpを投稿しました。

---
+++ challenge_8/insert_confirm.php
@@ -0,0 +1,101 @@
+<?php
+
+session_start();
+
+//名前
+if(empty($_POST['name'])){
+	$_SESSION['name'] = null;
+}else{
+	$_SESSION['name'] = $_POST['name'];
+	$name_i = $_SESSION['name'];
+}
+
+//生年月日：年
+if(empty($_POST['year'])){
+	$_SESSION['year'] = null;
+}else{
+	$_SESSION['year'] = $_POST['year'];
+	$year_i = $_SESSION['year'];
+}
+
+//生年月日：月
+if(empty($_POST['month'])){
+	$_SESSION['month'] = null;
+}else{
+	$_SESSION['month'] = $_POST['month'];
+	$month_i = $_SESSION['month'];
+}
+
+//生年月日：日
+if(empty($_POST['day'])){
+	$_SESSION['day'] = null;
+}else{
+	$_SESSION['day'] = $_POST['day'];
+	$day_i = $_SESSION['day'];
+}
+
+//電話番号
+if(empty($_POST['tell'])){
+	$_SESSION['tell'] = null;
+}else{
+	$_SESSION['tell'] = $_POST['tell'];
+	$tell_i = $_SESSION['tell'];
+}
+
+//種別
+if(empty($_POST['type'])){
+	$_SESSION['type'] = null;
+}else{
+	$_SESSION['type'] = $_POST['type'];
+	$type_i = $_SESSION['type'];
+}
+
+//自己紹介
+if(empty($_POST['comment'])){
+	$_SESSION['comment'] = null;
+}else{
+	$_SESSION['comment'] = $_POST['comment'];
+	$comment_i = $_SESSION['comment'];
+}
+
+if(empty($name_i)){
+	echo '名前が入力されていません。' . "<br>";
+}
+
+if(empty($year_i) || empty($month_i) || empty($day_i)){
+	echo '生年月日が選択されていません。' . "<br>";
+}
+
+if(empty($type_i)){
+	echo '種別が選択されていません。' . "<br>";
+}
+
+if(empty($tell_i)){
+	echo '電話番号が入力されていません。' . "<br>";
+}
+
+if(empty($comment_i)){
+	echo '自己紹介が入力されていません。' . "<br>" . "<br>";
+}
+
+if(empty($name_i) || empty($year_i) || empty($month_i) || empty($day_i) || empty($type_i) || empty($tell_i) || empty($comment_i)){
+?>
+	<a href="insert.php">戻る</a>
+<?php
+}else{
+	echo '確認画面' . "<br>" . "<br>";
+	echo '名前：' . $name_i . "<br>";
+	echo '生年月日：' . $year_i . '年' . $month_i . '月' . $day_i . '日' . "<br>";
+	if($type_i == 1){
+		echo '種別：エンジニア' . "<br>";
+	}elseif($type_i == 2){
+		echo '種別：営業' . "<br>";
+	}
+	echo '電話番号：' . $tell_i . "<br>";
+	echo '自己紹介：' . $comment_i . "<br>" . "<br>";
+	echo '上記の内容で登録いたします。よろしいですか？' . "<br>" . "<br>";
+?>
+<a href="insert_result.php">はい</a>　
+<a href="insert.php">いいえ</a>
+<?php
+}
