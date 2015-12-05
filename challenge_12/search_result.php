ec38622bb654e65641b39f0c8cb09e85172065a3
ryosuke <ryosukekt89@gmail.com>
2015-12-05 13:53:02 +0900

    search_result.phpを投稿しました。

---
+++ challenge_12/search_result.php
@@ -0,0 +1,47 @@
+<?php
+require_once '../common/defineUtil.php';
+require_once '../common/scriptUtil.php';
+require_once '../common/dbaccesUtil.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+      <title>検索結果画面</title>
+</head>
+    <body>
+        <h1>検索結果</h1>
+        <table border=1>
+            <tr>
+                <th>名前</th>
+                <th>生年</th>
+                <th>種別</th>
+                <th>登録日時</th>
+            </tr>
+        <?php
+
+
+
+        $result = null;
+        if(empty($_GET['name']) && empty($_GET['year']) && empty($_GET['type'])){
+            $result = serch_all_profiles();
+        }else{
+            $result = serch_profiles($_GET['name'],$_GET['year'],$_GET['type']);
+        }
+        foreach($result as $value){
+        ?>
+            <tr>
+                <td><a href="<?php echo RESULT_DETAIL ?>?id=<?php echo $value['userID']?>"><?php echo $value['name']; ?></a></td>
+                <!--生年月日が表示されていたのを生年のみが表示されるように修正-->
+                <td><?php echo date('Y年', strtotime($value['birthday'])); ?></td>
+                <td><?php echo ex_typenum($value['type']); ?></td>
+                <td><?php echo date('Y年n月j日　G時i分s秒', strtotime($value['newDate']));; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        </table>
+        <!--トップページへのリンクを追加-->
+        <?php echo return_top(); ?>
+</body>
+</html>
