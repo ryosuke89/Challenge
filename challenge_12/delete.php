a61408b299c1b5471d74dcbcd1a866a165bb867d
ryosuke <ryosukekt89@gmail.com>
2015-12-06 17:53:03 +0900

    delete.phpを追加しました。

--- challenge_12/delete.php
+++ challenge_12/delete.php
@@ -11,34 +11,43 @@ require_once '../common/dbaccesUtil.php';
 </head>
   <body>
     <?php
-    $result = profile_detail($_GET['id']);
-    //エラーが発生しなければ表示を行う
-    if(is_array($result)){
-    ?>
-    <h1>削除確認</h1>
-    <!--改行を追加-->
-    以下の内容を削除します。よろしいですか？<br>
-    名前:<?php echo $result[0]['name'];?><br>
-    生年月日:<?php echo $result[0]['birthday'];?><br>
-    種別:<?php echo ex_typenum($result[0]['type']);?><br>
-    電話番号:<?php echo $result[0]['tell'];?><br>
-    自己紹介:<?php echo $result[0]['comment'];?><br>
-    登録日時:<?php echo date('Y年n月j日　G時i分s秒', strtotime($result[0]['newDate'])); ?><br>
+    //詳細画面から「削除」ボタンを押した場合のみ処理を行う条件分岐を追加
+    if(empty($_POST['mode'])){
+        echo 'アクセスルートが不正です。もう一度トップページからやり直してください<br>';
+    }elseif($_POST['mode']=="SAKUJYO"){
 
-    <!--delete_result.phpのURLに該当するuserIDを追加する処理を追加-->
-    <form action="<?php echo DELETE_RESULT; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
-      <input type="submit" name="YES" value="はい"style="width:100px">
-    </form>
-    <!--改行を削除-->
-    <form action="<?php echo RESULT_DETAIL; ?>" method="POST">
-      <input type="submit" name="NO" value="詳細画面に戻る"style="width:100px">
-    </form>
+        $result = profile_detail($_GET['id']);
+        //エラーが発生しなければ表示を行う
+        if(is_array($result)){
+        ?>
+        <h1>削除確認</h1>
+        <!--改行を追加-->
+        以下の内容を削除します。よろしいですか？<br>
+        名前:<?php echo $result[0]['name'];?><br>
+        生年月日:<?php echo $result[0]['birthday'];?><br>
+        種別:<?php echo ex_typenum($result[0]['type']);?><br>
+        電話番号:<?php echo $result[0]['tell'];?><br>
+        自己紹介:<?php echo $result[0]['comment'];?><br>
+        登録日時:<?php echo date('Y年n月j日　G時i分s秒', strtotime($result[0]['newDate'])); ?><br>
 
-    <?php
-    }else{
-        echo 'データの取得に失敗しました。次記のエラーにより処理を中断します:'.$result;
+        <!--delete_result.phpのURLに該当するuserIDを追加する処理を追加-->
+        <form action="<?php echo DELETE_RESULT; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
+          <!--URLでdelete_result.phpに直接アクセスするとエラーが表示される処理を追加するためにhiddenを追加-->
+          <input type="hidden" name="mode"  value="SAKUJYO_RESULT">
+          <input type="submit" name="YES" value="はい"style="width:100px">
+        </form>
+        <!--改行を削除-->
+        <!--result_detail.phpのURLに該当するuserIDを追加する処理を追加-->
+        <form action="<?php echo RESULT_DETAIL; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
+          <input type="submit" name="NO" value="詳細画面に戻る"style="width:100px">
+        </form>
+
+        <?php
+        }else{
+            echo 'データの取得に失敗しました。次記のエラーにより処理を中断します:'.$result;
+        }
     }
-    echo return_top();
-    ?>
+        echo return_top();
+        ?>
    </body>
 </html>
