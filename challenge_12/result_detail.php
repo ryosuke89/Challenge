15757618b99b202506ef29757a4a4c1d3a010de1
ryosuke <ryosukekt89@gmail.com>
2015-12-07 10:42:18 +0900

    result_detail.phpを追加しました。

--- challenge_12/result_detail.php
+++ challenge_12/result_detail.php
@@ -12,38 +12,43 @@ require_once '../common/dbaccesUtil.php';
 </head>
   <body>
     <?php
-    $result = profile_detail($_GET['id']);
+    //userIDを指定せずURLで直接アクセスした場合にエラーが表示される処理を追加
+    if(!empty($_GET['id'])){
+        $result = profile_detail($_GET['id']);
 
-    //URLで直接アクセスした時にデータが存在しない場合、エラーが表示される処理を追加
-    if(!empty($result)){
-        //エラーが発生しなければ表示を行う
-        if(is_array($result)){
-        ?>
+        //URLで直接アクセスした時にデータが存在しない場合、エラーが表示される処理を追加
+        if(!empty($result)){
+            //エラーが発生しなければ表示を行う
+            if(is_array($result)){
+            ?>
 
-        <h1>詳細情報</h1>
-        名前:<?php echo $result[0]['name'];?><br>
-        生年月日:<?php echo $result[0]['birthday'];?><br>
-        種別:<?php echo ex_typenum($result[0]['type']);?><br>
-        電話番号:<?php echo $result[0]['tell'];?><br>
-        自己紹介:<?php echo $result[0]['comment'];?><br>
-        登録日時:<?php echo date('Y年n月j日　G時i分s秒', strtotime($result[0]['newDate'])); ?><br>
+            <h1>詳細情報</h1>
+            名前:<?php echo $result[0]['name'];?><br>
+            生年月日:<?php echo $result[0]['birthday'];?><br>
+            種別:<?php echo ex_typenum($result[0]['type']);?><br>
+            電話番号:<?php echo $result[0]['tell'];?><br>
+            自己紹介:<?php echo $result[0]['comment'];?><br>
+            登録日時:<?php echo date('Y年n月j日　G時i分s秒', strtotime($result[0]['newDate'])); ?><br>
 
-        <!--update.phpのURLに該当するuserIDを追加する処理を追加-->
-        <form action="<?php echo UPDATE; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
-            <!--URLでupdate.phpに直接アクセスするとエラーが表示される処理を追加するためにhiddenを追加-->
-            <input type="hidden" name="mode" value="KOUSHIN">
-            <input type="submit" name="update" value="変更" style="width:100px">
-        </form>
-        <!--delete.phpのURLに該当するuserIDを追加する処理を追加-->
-        <form action="<?php echo DELETE; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
-            <!--URLでdelete.phpに直接アクセスするとエラーが表示される処理を追加するためにhiddenを追加-->
-            <input type="hidden" name="mode" value="SAKUJYO">
-            <input type="submit" name="delete" value="削除"style="width:100px">
-        </form>
+            <!--update.phpのURLに該当するuserIDを追加する処理を追加-->
+            <form action="<?php echo UPDATE; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
+                <!--URLでupdate.phpに直接アクセスするとエラーが表示される処理を追加するためにhiddenを追加-->
+                <input type="hidden" name="mode" value="KOUSHIN">
+                <input type="submit" name="update" value="変更" style="width:100px">
+            </form>
+            <!--delete.phpのURLに該当するuserIDを追加する処理を追加-->
+            <form action="<?php echo DELETE; ?>?id=<?php echo $result[0]['userID']; ?>" method="POST">
+                <!--URLでdelete.phpに直接アクセスするとエラーが表示される処理を追加するためにhiddenを追加-->
+                <input type="hidden" name="mode" value="SAKUJYO">
+                <input type="submit" name="delete" value="削除"style="width:100px">
+            </form>
 
-        <?php
+            <?php
+            }else{
+                echo 'データの検索に失敗しました。次記のエラーにより処理を中断します:'.$result;
+            }
         }else{
-            echo 'データの検索に失敗しました。次記のエラーにより処理を中断します:'.$result;
+            echo 'データが存在しません。<br>';
         }
     }else{
         echo 'データが存在しません。<br>';
