2e42305263363d51b92fea30e89bf46a6550544d
ryosuke <ryosukekt89@gmail.com>
2015-12-08 10:43:02 +0900

    delete_result.phpを追加しました。

--- challenge_12/delete_result.php
+++ challenge_12/delete_result.php
@@ -11,20 +11,28 @@ require_once '../common/dbaccesUtil.php';
 </head>
 <body>
     <?php
+    //delete_result.phpへの遷移をログに出力する処理を追加
+    log_access(DELETE_RESULT);
     //削除確認画面から「はい」ボタンを押した場合のみ処理を行う条件分岐を追加
     if(empty($_POST['mode'])){
         echo 'アクセスルートが不正です。もう一度トップページからやり直してください<br>';
+        //URLで直接アクセスした場合にエラーをログに出力する処理を追加
+        log_error('アクセスルートが不正です。');
     }elseif($_POST['mode']=="SAKUJYO_RESULT"){
 
         $result = delete_profile($_GET['id']);
         //エラーが発生しなければ表示を行う
         if(!isset($result)){
+        //データを削除した場合にログに出力する処理を追加
+        log_syori('データを削除しました。');
         ?>
         <h1>削除確認</h1>
         削除しました。<br>
         <?php
         }else{
             echo 'データの削除に失敗しました。次記のエラーにより処理を中断します:'.$result;
+            //データの削除に失敗した場合にエラーをログに出力する処理を追加
+            log_error('データの削除に失敗しました。');
         }
     }
     echo return_top();
