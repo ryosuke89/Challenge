26d7c4ba02b196777de5b3f4fcd697e15800bc76
ryosuke <ryosukekt89@gmail.com>
2015-12-13 14:45:40 +0900

    7_13select_1challenge.phpを投稿しました。

---
+++ challenge_7/challenge_7_13/7_13select_1challenge.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+ </head>
+ <body>
+  <form action="./7_13select_2challenge.php" method="POST">
+  データの検索<br><br>
+  ID：<br>
+   <input type="text" name="profilesID"><br><br>
+  名前：<br>
+   <input type="text" name="name"><br><br>
+  電話番号：<br>
+   <input type="text" name="tell"><br><br>
+  年齢：<br>
+   <input type="text" name="age"><br><br>
+  生年月日：<br>
+   <input type="text" name="birthday"><br><br>
+   <input type="submit" name="btnSubmit">
+  </form>
+ </body>
+</html>
