b924877b78b022a338a4550123c23e0834e2e439
ryosuke <ryosukekt89@gmail.com>
2015-12-13 14:43:37 +0900

    7_13challenge.phpを投稿しました。

---
+++ challenge_7/challenge_7_13/7_13challenge.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+ </head>
+ <body>
+  操作の選択<br><br>
+  <a href="7_13create_1challenge.php">テーブルの作成</a>　　
+  <a href="7_13insert_1challenge.php">追加</a>　　
+  <a href="7_13select_1challenge.php">検索</a>　　
+  <a href="7_13delete_1challenge.php">削除</a>　　
+  <a href="7_13update_1challenge.php">更新</a>
+ </body>
+</html>
