4c6f2c04410b1b3edc879ebbd1c46fab35ecef34
ryosuke <ryosukekt89@gmail.com>
2015-12-13 14:44:09 +0900

    7_13create_1challenge.phpを投稿しました。

---
+++ challenge_7/challenge_7_13/7_13create_1challenge.php
@@ -0,0 +1,32 @@
+<html>
+ <head>
+ </head>
+ <body>
+  <form action="./7_13create_2challenge.php" method="POST">
+  テーブルの作成<br><br>
+  テーブル名：<br>
+   <input type="text" name="table"><br><br>
+  カラム名1：<br>
+   <input type="text" name="column1"><br><br>
+  データ型1：<br>
+   <input type="text" name="data1"><br><br>
+  カラム名2：<br>
+   <input type="text" name="column2"><br><br>
+  データ型2：<br>
+   <input type="text" name="data2"><br><br>
+  カラム名3：<br>
+   <input type="text" name="column3"><br><br>
+  データ型3：<br>
+   <input type="text" name="data3"><br><br>
+  カラム名4：<br>
+   <input type="text" name="column4"><br><br>
+  データ型4：<br>
+   <input type="text" name="data4"><br><br>
+  カラム名5：<br>
+   <input type="text" name="column5"><br><br>
+  データ型5：<br>
+   <input type="text" name="data5"><br><br>
+   <input type="submit" name="btnSubmit">
+  </form>
+ </body>
+</html>
