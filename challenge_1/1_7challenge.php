e9d4199fffdbb313027127af173df2ffaafb9915
ryosuke <ryosukekt89@gmail.com>
2016-01-31 13:50:39 +0900

    1_7challenge.phpを修正しました。

--- challenge_1/1_7challenge.php
+++ challenge_1/1_7challenge.php
@@ -2,26 +2,24 @@
 
 //①
 $syubetsu = $_GET['syubetsu'];
-if ($syubetsu == 1) {echo '雑貨';
-} elseif ($syubetsu == 2) {echo '生鮮食品';
-} elseif ($syubetsu == 3) {echo 'その他';
-};
-
-echo "<br>";
+if ($syubetsu == 1) {echo '商品種別：雑貨<br>';
+} elseif ($syubetsu == 2) {echo '商品種別：生鮮食品<br>';
+} elseif ($syubetsu == 3) {echo '商品種別：その他<br>';
+}
 
 //②
 $sougaku = $_GET['sougaku'];
 $kosuu = $_GET['kosuu'];
 
 //総額
-echo $sougaku ."<br>";
+echo '総額：' . $sougaku . '円<br>';
 
 //1個当たりの値段
-echo $sougaku / $kosuu ."<br>";
+echo '1個当たりの値段：' . $sougaku / $kosuu . '円<br>';
 
 //③
 if ($sougaku >= 3000 && $sougaku < 5000) {
-echo $sougaku * 0.04;
+echo 'ポイント：' . $sougaku * 0.04;
 } elseif ($sougaku >= 5000 ) {
-echo $sougaku * 0.05;
+echo 'ポイント：' . $sougaku * 0.05;
 }
