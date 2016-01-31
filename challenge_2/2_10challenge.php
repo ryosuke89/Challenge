06029844c17cb24e08e8ac6065506efe3f137948
ryosuke <ryosukekt89@gmail.com>
2016-01-31 13:55:18 +0900

    2_10challenge.phpを修正しました。

--- challenge_2/2_10challenge.php
+++ challenge_2/2_10challenge.php
@@ -1,52 +1,61 @@
 <?php
 
 //課題10
+//GETの取得
 $param = $_GET['param'];
 
 //元の値
-echo $param . "<br>";
+echo '元の値：<br>' . $param . "<br>";
+
+//カウント用
+$count_two = 0;
+$count_three = 0;
+$count_five = 0;
+$count_seven = 0;
+
+//2で割った数をカウント
+$key = $param;
+while ($key % 2 == 0) {
+    $key = $key / 2;
+    $count_two++;
+}
+
+//3で割った数をカウント
+while ($key % 3 == 0) {
+    $key = $key / 3;
+    $count_three++;
+}
+
+//5で割った数をカウント
+while ($key % 5 == 0) {
+    $key = $key / 5;
+    $count_five++;
+}
+
+//7で割った数をカウント
+while ($key % 7 == 0) {
+    $key = $key / 7;
+    $count_seven++;
+}
 
 //素因数分解の結果
-$key = $param;
-while ($key % 2 == 0 && $key > 2) {
-  switch($key) {
-     case $key:
-       $key = $key / 2;
-       echo 2 . ',';
-       break;
-  }
+if($count_two > 0 || $count_three > 0 || $count_five > 0 || $count_seven > 0){
+    echo '<br>1ケタの素因数：<br>';
+}
+if($count_two > 0){
+    echo '2の' . $count_two . '乗<br>';
+}
+if($count_three > 0){
+    echo '3の' . $count_three . '乗<br>';
+}
+if($count_five > 0){
+    echo '5の' . $count_five . '乗<br>';
+}
+if($count_seven > 0){
+    echo '7の' . $count_seven . '乗<br>';
 }
 
-while ($key % 3 == 0 && $key > 3) {
-  switch($key) {
-     case $key:
-       $key = $key / 3;
-       echo 3 . ',';
-       break;
-  }
-}
-
-while ($key % 5 == 0 && $key > 5) {
-  switch($key) {
-     case $key:
-       $key = $key / 5;
-       echo 5 . ',';
-       break;
-  }
-}
-
-while ($key % 7 == 0 && $key > 7) {
-  switch($key) {
-     case $key:
-       $key = $key / 7;
-       echo 7 . ',';
-       break;
-  }
-}
-
-echo $key . "<br>";
-
-//その他
-if ($key >= 10) {
-       echo '元の値1ケタの素因数その他';
+//割り切れなかった数
+if($key > 1){
+    echo '<br>その他：<br>' . $key;
 }
